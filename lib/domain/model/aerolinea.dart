e9662b86d4a45f8f2238c761e18f9fd0d288bf78
dylan18x <gadieldylan02@gmail.com>
2026-07-13 16:22:43 -0500

    CTA-31-SUBIDA DE FOTOS CON EXITO

--- lib/domain/model/aerolinea.dart
+++ lib/domain/model/aerolinea.dart
@@ -4,17 +4,20 @@ class Aerolinea {
   final int    idAerolinea;
   final String nombre;
   final String pais;
+  final String? imageUrl;
 
   const Aerolinea({
     required this.idAerolinea,
     required this.nombre,
     required this.pais,
+    this.imageUrl,
   });
 
   factory Aerolinea.fromJson(Map<String, dynamic> j) => Aerolinea(
     idAerolinea: j['id_aerolinea'] as int,
     nombre:      j['nombre']       as String,
     pais:        j['pais']         as String,
+    imageUrl: j['image_url'] as String?,
   );
 
   Map<String, dynamic> toJson() => {
@@ -22,9 +25,11 @@ class Aerolinea {
     'pais':   pais,
   };
 
-  Aerolinea copyWith({String? nombre, String? pais}) => Aerolinea(
+  Aerolinea copyWith({String? nombre, String? pais,String? imageUrl,}) => Aerolinea(
     idAerolinea: idAerolinea,
     nombre:      nombre ?? this.nombre,
     pais:        pais   ?? this.pais,
+    imageUrl: imageUrl ?? this.imageUrl,
+    
   );
 }
